3eaaa35723a31cd05b85cce1a381a14edbd8e8c4
canh96 <ehdgus662@gmail.com>
2022-05-03 11:48:48 +0900

    220503_점심 전 수업

---
+++ basic/14.star_print.py
@@ -0,0 +1,8 @@
+#*찍기
+for x in range(1, 6):
+    for y in range(x, 6):
+        print('*', end='')  # end =
+    print('')
+
+print('hello', end= '')   # end=''는 밑에 글을 붙어준다
+print('world')
--- 수업/datatype02.py
+++
@@ -1,4 +0,0 @@
-#문자열
-bruce_eckel = 'life is short,\n\t you need Python'
-print(bruce_eckel)
-
