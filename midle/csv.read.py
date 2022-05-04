fce41af010fe9ae6c6f2cf864e5bc69564923b8d
canh96 <ehdgus662@gmail.com>
2022-05-04 11:44:49 +0900

    커밋

---
+++ midle/csv.read.py
@@ -0,0 +1,13 @@
+#csv파일 읽기
+import csv
+
+file_name = 'busanbus.csv'
+
+f = open('file_name, mode = 'r', encoding = 'cp949')
+reader = csv.reader(f, delemiter=',') # 구분자가 , 일 경우
+
+next(reader) 제목줄 있을때 필요없을 경우
+for line in reader
+    print(line)
+
+f.close()
